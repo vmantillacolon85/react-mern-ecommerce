1790c8d235f18a7db96de740c223b15b5329ec67
Victor Mantilla Colon <vmcolon@Victors-Air.home>
2021-04-01 16:24:47 -0400

    adding initial files

---
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function UploadProductPage() {
+    return (
+        <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
+            <div style={{ textAlign: "center", marginBottom: "2rem"}}>
+                <h2> Upload Travel Product</h2>
+            </div>
+            UploadProductPage
+        </div>
+    )
+}
+
+export default UploadProductPage
